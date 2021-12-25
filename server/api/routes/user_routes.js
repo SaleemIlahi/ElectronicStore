5dec60246d28f829a85e880035e2b64ba9837b3a
Mohamed Saleem <saleemilahi21@gmail.com>
2021-12-25 18:43:02 +0530

    user authencation middleware added to routes

--- server/api/routes/user_routes.js
+++ server/api/routes/user_routes.js
@@ -2,9 +2,18 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/user_controller.js')
+const { userAuth } = require('../middlewares/userAuth.js')
+// const { verifyMailID } = require('../helpers/verify_mail_id.js')
 
-router.post('/api/v1/register',userController.register)
-router.post('/api/v1/login',userController.login)
+router.post('/api/v1/register', userController.register)
+router.post('/api/v1/login', userController.login)
+router.get('/api/v1/home', userAuth, (req, res) => {
+    const user = req.user
+    res.json({
+        user
+    })
+})
+// router.get('/api/v1/verify-email',verifyMailID)
 
 
 module.exports = router
