ac551736c50c7b7acec31d91b1ac74ad078123a2
Mohamed Saleem <saleemilahi21@gmail.com>
2022-03-22 11:44:27 +0530

    Deploying on Heroku

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const productRouter = require('./api/routes/product_routes.js')
 const { errorHandler } = require('./api/middlewares/errorHandler.js')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
+const path = require('path')
 
 const app = express()
 
@@ -29,10 +30,17 @@ app.use('/',userRouter)
 app.use('/',productRouter)
 app.use(errorHandler)
 
-
+// Serve static file in production
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join("client/build")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client","build", "index.html"));
+    });
+  }
 
 // port config
-const port = process.env.PORT
+const port = process.env.PORT || 8000
 
 // Server Listening
 app.listen(port, () => console.log(`Server Listening at http://localhost:${port}`))
