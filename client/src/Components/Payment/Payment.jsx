da27ec80640b52166eaf4ff906b0587a62da99bf
Mohamed Saleem <saleemilahi21@gmail.com>
2022-04-15 16:35:03 +0530

    integrated stripe payment gateway

---
+++ client/src/Components/Payment/Payment.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { useLocation } from 'react-router-dom'
+
+import CheckoutForm from "./CheckoutForm";
+import "./payment.css";
+
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+// This is your test publishable API key.
+const stripePublicKey = process.env.REACT_APP_STRIPE_PUBLIC_KEY
+const stripePromise = loadStripe(stripePublicKey);
+
+export default function Payment() {
+    const [clientSecret, setClientSecret] = useState("");
+    const location = useLocation()
+
+    const totalPrice = location.state
+
+    useEffect(() => {
+        // Create PaymentIntent as soon as the page loads
+        fetch("/api/v1/checkout", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ items: {price: totalPrice * 100} }),
+        })
+            .then((res) => res.json())
+            .then((data) => setClientSecret(data.clientSecret));
+    },[totalPrice]);
+
+    const appearance = {
+        theme: 'stripe',
+    };
+    const options = {
+        clientSecret,
+        appearance,
+    };
+
+    return (
+        <div className="App">
+            {clientSecret && (
+                <Elements options={options} stripe={stripePromise}>
+                    <CheckoutForm />
+                </Elements>
+            )}
+        </div>
+    );
+}
---
+++ server/api/services/payment_service.js
@@ -0,0 +1,33 @@
+
+// This is your test secret API key.
+const stripeKey = process.env.STRIPE_KEY
+const stripe = require("stripe")(stripeKey);
+
+// app.use(express.static("public"));
+// app.use(express.json());
+
+const calculateOrderAmount = (items) => {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // people from directly manipulating the amount on the client
+  return items;
+};
+
+const payment = async (req, res) => {
+  const { items } = req.body;
+
+  // Create a PaymentIntent with the order amount and currency
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: calculateOrderAmount(items.price),
+    currency: "inr",
+    automatic_payment_methods: {
+      enabled: true,
+    }
+  });
+
+  res.send({
+    clientSecret: paymentIntent.client_secret,
+  });
+}
+
+module.exports = payment
